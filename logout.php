4885a0a8396eee31b73ee338fdab96f0811d4c91
JhonataLacerda <jonata_5000@hotmail.com>
2014-09-24 15:42:34 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
-﻿
+ï»¿
 <?php
-// Destrui a sess�o existentes//
+// Destruira sessão existentes//
 session_start("AB");
 session_unset();
 echo "<meta http-equiv='refresh' content='0.1; URL=index.html'>";
